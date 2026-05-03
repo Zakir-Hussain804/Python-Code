467a6ea073386fd99b26094b9334ea2256613f9c
unknown <zakirhussain804n@gmail.com>
2026-05-04 00:44:11 +0500

    Leature1

---
+++ Leature_1.py
@@ -0,0 +1,52 @@
+#variable declaration
+FirstName="Zakir" #using styel Camel Case
+LastName="Hussain"
+age=21
+github="Follow me"
+print("Name :",FirstName,LastName,"\n","Age :" ,age, "\n","Github :",github )
+#DATA type of Python 
+
+a="hello word"
+b=19
+c=True
+d=223.3
+
+print(type(a)) #check a data type (str)
+print(type(b)) #check a data type (int)
+print(type(c)) #check a data type (bool)
+print(type(d)) #check a data type (float)
+
+
+# Opeartor 
+#Arithmetice Operator
+num1=40
+num2=4
+
+print("add :",num1+num1)
+print("sub :",num1-num2)
+print("mult",num1*num2)
+print("division :" ,num1/num2)
+print("Module :" ,num1%num2)
+print("power",num1**num2) #num1p power num2 
+
+#Relational /Comparison
+print (num1>num2) #num1 gearter num2 :true
+print (num1<num2) #num1 smaller num2 :flase
+print (num1==num2) #num1 equal num2 :flase
+print (num1!=num2) #num1 not equal num2 :true
+ 
+#Assigement
+n=10
+n=n+3
+print(n)
+n=+5 
+print(n)
+n -=5
+print(n)
+
+#locatOperator (not and or)
+print(not (30>2)) #false
+print(not (1>10)) #true
+print ((20>10) and (1>0)) # all condtion  (true )true
+print ((10>20) and (1>0)) # false  
+print ((10>20) and (1>0)) # false 
