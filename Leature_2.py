503e70bceb7881dd2fb7fca088c1c2380f12f45c
unknown <zakirhussain804n@gmail.com>
2026-05-04 12:40:07 +0500

    upadata code

--- Leature_2.py
+++ Leature_2.py
@@ -5,5 +5,12 @@
 #Jab different types use hote hain
 x=10
 y=3.2
+print(float(x)) # int convert to float
+print(type(x)) # float 
+print (str(x)) #int covert to String 
+print(type(x)) #String
 print(int(y))  # float  convert to int 
-print(float(x)) # int convert to floa
+z="100"
+print(int(z)) #String convert to int 
+print(type(z)) #int 
+
