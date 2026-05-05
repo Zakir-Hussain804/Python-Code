a361d628fbb72efcf06c5b61f34b229dc04e328b
unknown <zakirhussain804n@gmail.com>
2026-05-05 15:23:39 +0500

    Check user Login enter username and password

--- Leature_3.py
+++ Leature_3.py
@@ -1,9 +1,21 @@
 #Condition Statement  :if elif else
 #Check User Age 
 age=int(input("Enter Age :"))
+#age=18
 if age<13:
     print("Child")
 elif age >= 13 and age < 18:
     print("Teenager")
 else:
-    print("adult") 
+    print("adult")
+
+# Check user Login enter username and password 
+userName=input("userName :")
+password=input("passoword :")
+
+if (userName=="user" and password=="pass"):
+    print("Login SuccessFully")
+elif userName !="user":
+    print("Wrong userName")
+else :
+    print("Wrong user and Password")
