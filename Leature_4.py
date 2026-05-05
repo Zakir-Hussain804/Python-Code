017e27a5ffcf5698f6c0e358d3ac764a7ce524e8
unknown <zakirhussain804n@gmail.com>
2026-05-05 17:36:21 +0500

    Python while Loop Partices

---
+++ Leature_4.py
@@ -0,0 +1,18 @@
+#Loop 1 while Loop
+
+count=0
+while count<5:
+    print("hello")
+    count +=1
+# Table 
+n=int(input("Enter the any Number :"))
+i=1
+while i<10 :
+    print( n,"x",i ,"=" , n*i )
+    i+=1
+
+print("Table is Reverser")
+j=10
+while j>1 :
+    print( n,"x",j ,"=" , n*j )
+    j-=1
