7cce62b65ab117d0a7fcd0258bb2bc4b926f5231
unknown <zakirhussain804n@gmail.com>
2026-05-05 15:08:46 +0500

    Conditon Statement : if elif and else

---
+++ Leature_3.py
@@ -0,0 +1,9 @@
+#Condition Statement  :if elif else
+#Check User Age 
+age=int(input("Enter Age :"))
+if age<13:
+    print("Child")
+elif age >= 13 and age < 18:
+    print("Teenager")
+else:
+    print("adult") 
