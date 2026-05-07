fe0788914e2392d2885a8c349e86bb1a0a415587
unknown <zakirhussain804n@gmail.com>
2026-05-07 23:24:14 +0500

    for loop topic

--- Leature_5.py
+++ Leature_5.py
@@ -1,2 +1,12 @@
 # Topice of For Loop 
 var  ="hello"
+# for l in var :
+#     print(l)
+
+
+# check o include ya hai ya is string  use karta hai in operator
+
+if "u" in var :
+    print("include the a o")
+else :
+ print("not inculde ")
