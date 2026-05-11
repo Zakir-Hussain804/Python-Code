7ce696b5ca9b9e3d1b1590f66259986202e05a4f
unknown <zakirhussain804n@gmail.com>
2026-05-11 15:14:46 +0500

    updata the code

--- Leature_5.py
+++ Leature_5.py
@@ -1,13 +1,17 @@
 # Topice of For Loop 
 var  ="hello"
-# for l in var :
-#     print(l)
+for l in var :
+    print(l)
 
 
 # check o include ya hai ya is string  use karta hai in operator
 
-# if "u" in var :
-#     print("include the a o")
-# else :
-#  print("not inculde ")
-
+if "u" in var :
+    print("include the a o")
+else :
+ print("not inculde ")
+#sum of 1 to 10
+sum=0
+for i in range(0,10):
+    sum+=i
+print(sum)
