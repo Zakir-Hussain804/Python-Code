7fe8c0d6e6804e45e9541ad05ddc39a00c47c0b5
unknown <zakirhussain804n@gmail.com>
2026-05-14 01:42:06 +0500

    List is used to store the multiple value single variable ,List access , check type length

--- Leature_7.py
+++ Leature_7.py
@@ -1,7 +1,17 @@
-#String
-word="Python"
-print(word[4])
-for i in word:
-    print(i)
-txt = "The best things in life are free!"
-print ("free"in txt)
+# p
+
+# String Slicing mean sub string 
+var="I am Study Apna College"
+print(var[3:10:])
+
+#String Formating 
+a=10
+b=4
+sum=a+b
+
+print("sum is {}".format(sum))
+
+# Concatenate Sting
+word1="hello"
+word2="word"
+print(word1+" "+word2)
---
+++ Leature_8.py
@@ -0,0 +1,11 @@
+# topice List mutable sequence value
+# List is used to store the multiple value in single variable
+marks=[89,66,77,90,66]
+print(marks)
+print("Length Marks List =",len(marks))
+print('the value of this index marks[3]=',marks[3])
+marks[3]=55
+print("print marks List use a for ")
+print('replace the value of this index marks[3]',marks[3])
+for ch in marks:
+    print(ch)
