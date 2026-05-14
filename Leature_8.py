692ff895311efe6c5f5da4a1f1313d40c286d13a
unknown <zakirhussain804n@gmail.com>
2026-05-14 13:46:10 +0500

    updata a Code

--- Leature_8.py
+++ Leature_8.py
@@ -28,3 +28,18 @@ List1.sort()
 print("Sort of a List  = {}".format(List1))
 List1.reverse()
 print("reverse of a List  = {}".format(List1))
+List1.remove(1)
+print("remove for of this  1  = {}".format(List1))
+# Remove Specified Index
+List1.pop(4)
+print(" Remove Specified Index 4  = {}".format(List1))
+
+list2=[1,2,35,22,11,233,443,9]
+x=233
+idx=0
+for var in list2 :
+   if(var==x):
+       print(idx)
+       break
+   idx+=1
+
