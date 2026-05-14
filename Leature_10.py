f17545353b9d7cf0abb446531c04c162dea6a29d
unknown <zakirhussain804n@gmail.com>
2026-05-15 03:44:43 +0500

    method dictionaries

--- Leature_10.py
+++ Leature_10.py
@@ -7,8 +7,30 @@ studentInfo={
      "MisId":39300,
 }
 print("Type of =",type(studentInfo))
+#dictionaries convet to list
+list1=list(studentInfo)
+print(type(list1))
+#dictionaries convet to tuple
+tuple1=tuple(studentInfo)
+print(type(tuple1))
 #print one value
 print("Fname = ",studentInfo["Fname"])
 #change value
 studentInfo["Fname"]="Zakir Ali"
 print(" Change Fname = ",studentInfo["Fname"])
+# print(studentInfo) print all vlaue
+for ch in studentInfo:
+    print(ch," = ",studentInfo[ch])
+# Dictionaries Method 
+#d.keys return all the key
+key_Student=studentInfo.keys()
+print("key =",key_Student)
+
+#d.values retrun all vlaue
+value_Student=studentInfo.values()
+print("all values = ",value_Student)
+# updata all value 
+studentInfo.update({
+    'city':"isbamabad"
+})
+print("updata ",studentInfo)
