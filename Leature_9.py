94144d1c03eb6607b510f3b291b2d656aead62ae
unknown <zakirhussain804n@gmail.com>
2026-05-15 02:33:26 +0500

    updata the tuple

--- Leature_9.py
+++ Leature_9.py
@@ -6,3 +6,21 @@ tup2=(4) #single value of not a tuple
 print('type =',type(tup2))
 #tup[3]=3 not allow
 print(tup[3])
+#Slicing a Tuple
+print(tup[1:4])
+
+#sum all value in the tuple
+sum=0
+for va in tup:
+    sum +=va
+print("Tuple all value of Sum {}".format(sum))
+
+#updata tuple  or change value of tuble 
+tup3=("apple", "banana", "cherry")
+print("before tup3 =",tup3)
+#tuple convert to list
+x=list(tup3)
+x[2]="kiwi"
+#list convert to tuple
+tup3=tuple(x)
+print("after tup3 =",tup3)
